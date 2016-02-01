22b58097c62bf228aefccd26cc26e0b187ceb121
nfamula <nfamula@gmail.com>
2016-02-01 21:47:56 +0000

    13

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 RottenMangoes::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  resources :movies
+  
+  resources :movies do
+  resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
