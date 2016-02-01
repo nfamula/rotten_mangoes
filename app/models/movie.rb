9f68b54a3065ffd7df9fb66b48d5ae6962768fb7
nfamula <nfamula@gmail.com>
2016-02-01 22:22:15 +0000

    16

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,6 +21,14 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_past
 
+  def review_average
+    if :rating_out_of_ten == nil
+      puts "this movie has no reviews!"
+    else
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    end
+  end
+
   protected
 
   def release_date_is_in_the_past
